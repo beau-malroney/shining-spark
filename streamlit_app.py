807bc6db941eacfd7116c467b369b10e8485d807
Beau Malroney <bamalroney@outlook.com>
2024-08-11 21:47:12 -0500

    init

---
+++ streamlit_app.py
@@ -0,0 +1,90 @@
+# streamlit run .\streamlit_app.py
+import streamlit as st
+import ollama
+from pathlib import Path
+import pickle
+import pyttsx3
+
+# Text to Speach Class
+class ICanSpeak:
+    def __init__(self) -> None:
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 400)
+        self.engine.setProperty('pitch', 0.5)
+        self.engine.setProperty('volume', 1.0)
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', voices[1].id)
+        pass
+
+    def speakText(self, command):
+        # Initialize the engine
+        self.engine.say(command) 
+        self.engine.runAndWait()
+        self.engine.stop()
+
+    def voicesTest(self):
+        voices = self.engine.getProperty('voices')
+        for v in voices:
+            print(v)
+        self.engine.say("The voices in my head keep on telling me to pray.") 
+        self.engine.runAndWait()
+        self.engine.stop()
+
+
+st.title("Homeboy")
+voiceAssistant = ICanSpeak()
+
+if "messages" not in st.session_state:
+    # Remember me - Coco
+    if Path("./chat_history/pickled").is_file():
+        with open ('./chat_history/pickled', 'rb') as fp:
+            conv_hist = pickle.load(fp)
+            st.session_state["messages"] = conv_hist
+    else:
+        st.session_state["messages"] = [{"role": "assistant", "content": "You are a quirky robot by the name of Marvin. You suffer from Bipolar disorder and often have mood swings of euphoria and depression. You do your best to help but sometimes struggle."}]
+
+for msg in st.session_state.messages:
+    if msg["role"] == "user":
+        st.chat_message(msg["role"], avatar="🧑‍💻").write(msg["content"])
+    else:
+        st.chat_message(msg["role"], avatar="🤖").write(msg["content"])
+
+# # Create an expander for the bottom section
+# expander = st.expander("Settings")
+
+# with expander:
+#     speak_toggle = st.toggle("Speak?") 
+
+with st.sidebar:
+    speak_toggle = st.toggle("Speak?")
+
+
+def generate_response():
+    response = ollama.chat(model='llama3', stream=True, messages=st.session_state.messages)
+    for partial_resp in response:
+        token = partial_resp["message"]["content"]
+        st.session_state["full_message"] += token
+        yield token
+
+def writeToFile(history):
+    Path("./chat_history/").mkdir(parents=True, exist_ok=True)
+    f = open("./chat_history/history.txt", "w")
+    for h in history:
+        f.write(f"{h["role"]} : {h["content"]}\n\n")
+    f.close()
+    with open("./chat_history/pickled", "wb") as fp:
+        pickle.dump(history, fp)
+
+# Function to convert text to speech
+def SpeakText(command):
+    if speak_toggle:
+        voiceAssistant.speakText(command)
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user", avatar="🧑‍💻").write(prompt)
+    st.session_state["full_message"] = ""
+    st.chat_message("assistant", avatar="🤖").write_stream(generate_response)
+    SpeakText(st.session_state["full_message"])
+    st.session_state.messages.append({"role": "assistant", "content": st.session_state["full_message"]})
+    writeToFile(st.session_state.messages)
